504b2d417eaf7c547ca6baebb1bd08580bf7cf4d
createdbyharsh <createdbyharsh@gmail.comgit config --global user.email createdbyharsh@gmail.com>
2025-07-18 13:57:07 +0530

    made changes in createarea.jsx

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 
 function CreateArea() {
+  const [input, setInput] = useState("")
+  const [title, setTitle] = useState([]);
+
+  function handleChange(event){
+    setInput(event.target.value)
+    
+  }
+
+  function handleClick(event){
+    event.preventDefault();
+    setTitle((prevItems)=>{
+      return [...prevItems, input];
+    })
+    setInput("")
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
+        <input name="title" placeholder="Title" onChange={handleChange} value = {input}/>
         <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <button onClick = {handleClick}>Add</button>
       </form>
     </div>
   );
